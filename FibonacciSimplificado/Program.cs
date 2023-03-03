150fa1896aa27830fe76100f2b88dd3dac945221
Kichinda34 <leandroschneiderlima@gmail.com>
2023-03-03 14:05:34 -0300

    Add project files.

---
+++ FibonacciSimplificado/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double ant = 1, prox = 0;
+        int posicao;
+        Console.WriteLine("Escreva qual posição da sequência de Fibonacci deseja ver: ");
+        posicao = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < posicao; i++)
+        {
+            prox = prox + ant;
+            ant = prox - ant;
+            Console.WriteLine("O valor da posição desejada de Fibonacci é: " +prox) ;
+        }
+    }
+}
